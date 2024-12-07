cab388872e407c4e9218ff07d3edb8605f35456d
amnajee-art <aftabamna13@gmail.com>
2024-12-08 04:16:37 +0500

    first commit

---
+++ src/components/Helper/Section1.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+export default function Section1(){
+    return(
+        <div>
+
+            <div className=" md:ml-0 w-full md:w-full h-[500px] bg-[#00b0d7] flex ">
+                <div className="ml-10 md:ml-72 mt-40 text-white">
+                    <p>SUMMER 2024</p>
+                    <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold  mt-10">NEW COLLECTION</h1>
+                    <p className="mt-10 ">We know how large objects will act,</p>
+                    <p>but things on a small scale</p>
+                    <Button variant={"destructive"} className="mt-6 font-bold">SHOP NOW</Button>
+                </div>
+                <Image src="/section1img.jpg" alt="" width={400} height={400}
+                className="w-[600px] hidden md:block"/> 
+            
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Helper/menu.tsx
@@ -0,0 +1,60 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { MenuIcon, Search, User, ShoppingCartIcon,HeartIcon  } from "lucide-react"
+import Link from "next/link"
+  
+export default function Menu(){
+    return (
+        <div className="md:hidden">
+            <Dialog >
+  <DialogTrigger>
+        <MenuIcon className="text-[#00b0d7]"/>
+  </DialogTrigger>
+  <DialogContent>
+  <h1 className="text-2xl text-[#252B42] underline">Menu</h1>
+  {/* <ul className="block gap-10   font font-semibold cursor-pointer "> */}
+  <ul className="   md:hidden gap-10 font font-semibold cursor-pointer md:ml-48">
+                    <li className="text-[#252B42] mt-2"><Link href="/">Home</Link></li>
+                    <li><div className="">
+                        <select className="text-[#252B42] mt-2" name="" id="">
+                            <option value="">Shop</option>
+                            <option value="">Visit</option>
+                            
+                        </select>
+                    </div></li>
+                    <li className="text-[#252B42] mt-2"><Link href="/">About</Link></li>
+                    <li className="text-[#252B42] mt-2"><Link href="/">Blog</Link></li>
+                    <li className="text-[#252B42] mt-2"><Link href="/">Contact</Link></li>
+                    <li className="text-[#252B42] mt-2"><Link href="/">Pages</Link></li>
+                </ul>
+                <div className=" mt-2 md:ml-36 ml- text-[#00b0d7]">
+                <User/>
+                <p className=" mt-1">Login/Register</p>
+            </div>
+            {/* icons */}
+            <div className=" items-center md:space-x-6 space-x-1 "> 
+
+                <Search className="ml-4 text-[#00b0d7]"/>
+                <div className="flex text-[#00b0d7] ">
+                    <ShoppingCartIcon className=" mt-2 ml-2"/>
+                    <p>1</p>
+                    </div>
+               <div className="flex text-[#00b0d7] ">
+               <HeartIcon size={26} cursor={"pointer"} className="mt-2 ml-2"/>
+               <p className=" md:block">1</p>
+               </div>
+               </div>
+                
+               
+  </DialogContent>
+</Dialog>
+
+        </div>
+    )
+}
